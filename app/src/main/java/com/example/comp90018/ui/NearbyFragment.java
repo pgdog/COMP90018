4c4230cbeced268145941105aa6b97bdba78e3d0
Glutamine525 <az009931@vip.qq.com>
2020-11-05 20:41:27 +0800

    fix weather

--- app/src/main/java/com/example/comp90018/ui/NearbyFragment.java
+++ app/src/main/java/com/example/comp90018/ui/NearbyFragment.java
@@ -1,9 +1,15 @@
 package com.example.comp90018.ui;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -14,7 +20,17 @@ import androidx.fragment.app.Fragment;
 //import com.android.volley.VolleyError;
 //import com.android.volley.toolbox.JsonObjectRequest;
 //import com.android.volley.toolbox.Volley;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.comp90018.R;
+import com.example.comp90018.utils.DataManager;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,95 +39,73 @@ import org.json.JSONObject;
 
 public class NearbyFragment extends Fragment {
 
-
     private View view;
-
-
     private TextView weather;
-    private String test;
+    private ImageView weatherIcon;
 
+    private double longitude;
+    private double latitude;
+
+    private final LocationListener locationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+        }
+    };
 
     public NearbyFragment() {
         // Required empty public constructor
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-
-
-        view= inflater.inflate(R.layout.fragment_nearby, container, false);
-
+        view = inflater.inflate(R.layout.fragment_nearby, container, false);
         initView();
-
-
         return view;
     }
 
-
-    public void initView(){
-
-//        weather=(TextView)view.findViewById(R.id.nearby_weather);
-//        test = "no change";
-//
-//        String url = "http://api.openweathermap.org/data/2.5/weather?q=Norheimsund&appid=55f0e84cc2fb75990bf6252f08651ee8&units=metric";
-//
-//        JsonObjectRequest jsondata = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-//            @Override
-//            public void onResponse(JSONObject response) {
-//
-//
-//                try {
-//                    JSONObject main_object = response.getJSONObject("main");
-//                    //Select the object in the JSON
-//                    JSONArray array = response.getJSONArray("weather");
-//                    JSONObject obj = array.getJSONObject(0);
-//                    String temp = String.valueOf(main_object.getDouble("temp"));
-//                    String description = obj.getString("description");
-//                    String city = response.getString("name");
-//
-//
-//                    test = temp;
-//
-//
-//
-//                }catch (JSONException e){
-//                    e.printStackTrace();
-//                }
-//
-//            }
-//        }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//
-//            }
-//        }
-//
-//        );
-//        RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
-//        queue.add(jsondata);
-//
-//
-//        weather.setText(test);
-
-
+    public void initView() {
+        LocationManager lm = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 10, locationListener);
     }
 
-
-
-
-    public void get_weather(){
-
-
+    public void updateWeather(){
+        weather = (TextView) view.findViewById(R.id.nearby_weather);
+        weatherIcon = (ImageView) view.findViewById(R.id.nearby_weather_icon);
+        String url = String.format("http://api.openweathermap.org/data/2.5/find?lat=%s&lon=%s&appid=55f0e84cc2fb75990bf6252f08651ee8&units=metric", latitude, longitude);
+        JsonObjectRequest jsondata = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject main_object = response.getJSONObject("main");
+                    //Select the object in the JSON
+                    JSONArray array = response.getJSONArray("weather");
+                    JSONObject obj = array.getJSONObject(0);
+                    String temp = String.valueOf(main_object.getDouble("temp"));
+                    String description = obj.getString("description");
+                    String city = response.getString("name");
+                    String icon = obj.getString("icon").substring(0,2)+"d";
+                    weather.setText(String.format("%s: %s℃", city, temp));
+                    Picasso.get().load("https://openweathermap.org/img/w/" + icon + ".png").into(weatherIcon);
+                    Log.i("weather api response", temp);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("weather api error", error.toString());
+            }
+        });
+        RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        queue.add(jsondata);
     }
-
-
-
 
 }
